4bf2465c4d69b43f1c1126418f5ef53e0003c821
Mikhail Smirnov <mybestdog@gmail.com>
2018-04-24 11:10:36 +0600

    hw12

---
+++ HomeWork12/HomeWork12/Migrations/201804240506441_AddMigration1.cs
@@ -0,0 +1,26 @@
+namespace HomeWork12.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMigration1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+        }
+    }
+}
---
+++ HomeWork12/HomeWork12/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork12
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public StudentContext() : base("Homework12")
+        { }
+    }
+
+}
