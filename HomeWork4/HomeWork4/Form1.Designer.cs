433d6ccfa4e402127299055530f116b253c17cfb
Mikhail Smirnov <mybestdog@gmail.com>
2018-04-24 09:06:41 +0600

    hw4

---
+++ HomeWork4/HomeWork4/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace HomeWork4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.loginlabel = new System.Windows.Forms.Label();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.passwordlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Log In to DataBase";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // loginBox
+            // 
+            this.loginBox.Location = new System.Drawing.Point(28, 45);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(228, 20);
+            this.loginBox.TabIndex = 1;
+            // 
+            // loginlabel
+            // 
+            this.loginlabel.AutoSize = true;
+            this.loginlabel.Location = new System.Drawing.Point(135, 26);
+            this.loginlabel.Name = "loginlabel";
+            this.loginlabel.Size = new System.Drawing.Size(33, 13);
+            this.loginlabel.TabIndex = 2;
+            this.loginlabel.Text = "Login";
+            this.loginlabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(28, 94);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(228, 20);
+            this.passwordBox.TabIndex = 1;
+            // 
+            // passwordlabel
+            // 
+            this.passwordlabel.AutoSize = true;
+            this.passwordlabel.Location = new System.Drawing.Point(135, 75);
+            this.passwordlabel.Name = "passwordlabel";
+            this.passwordlabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordlabel.TabIndex = 2;
+            this.passwordlabel.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 188);
+            this.Controls.Add(this.passwordlabel);
+            this.Controls.Add(this.loginlabel);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.loginBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.Label loginlabel;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label passwordlabel;
+    }
+}
+
---
+++ HomeWork4/HomeWork4/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HomeWork4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder builder =new SqlConnectionStringBuilder();
+            builder.ConnectionString = "server=(local);initial catalog=MyDB";
+            builder.UserID = loginBox.Text;
+            builder.Password = passwordBox.Text;
+
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Вы подкючены!", "Успешно!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    connection.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("неверный логин или пароль", "Ошибка!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                   
+                }
+                
+            }
+        }
+      
+    }
+}
